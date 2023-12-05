5060af2aea4a49433025fddca2883ad2c9e9aafa
Lucas <lucasaxelson2016@gmail.com>
2023-12-05 22:03:20 +0000

    CMS - skeleton

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+    require("includes/server.php");
+    require("includes/header.php");
+?>
+
+<body>
+
+    <!-- Navigation -->
+    <?php
+        require("includes/navigation.php");
+    ?>
+
+    <!-- Page Content -->
+    <div class="container">
+
+        <div class="row">
+
+            <!-- Blog Entries Column -->
+            <?php
+                require("includes/blog.php")
+            ?>
+
+            <!-- Blog Sidebar Widgets Column -->
+            <?php
+                require("includes/sidebar.php")
+            ?>
+            
+        </div>
+        <!-- /.row -->
+
+        <hr>
+
+
+<?php
+    require("includes/footer.php")
+?>
